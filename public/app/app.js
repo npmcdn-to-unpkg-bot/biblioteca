6bf6b7810bdeb3a285023bead76e9c3ae14270ae
Haroldo Ramirez da Nóbrega <haroldoramirez@gmail.com>
2016-02-02 10:03:04 -0200

    adicionado menu angular material lite

--- public/app/app.js
+++ public/app/app.js
@@ -2,13 +2,7 @@ angular
     .module
         ('architectplay',
             ['ngRoute',
-             'ngResource',
-             'ui.bootstrap',
-             'toastr',
-             'ngAnimate',
-             'angular-loading-bar',
-             'mgcrea.ngStrap.datepicker',
-             'ui.utils.masks'
+             'ngResource'
             ]
         )
     .config(function ($routeProvider) {
@@ -39,37 +33,4 @@ angular
                 templateUrl: '/assets/app/views/usuarios/list.html',
                 controller: 'usuario.list.controller'
             });
-   }).config(function(toastrConfig) {
-     angular.extend(toastrConfig, {
-        allowHtml: false,
-        autoDismiss: false,
-        closeButton: true,
-        closeHtml: '<button>&times;</button>',
-        containerId: 'toast-container',
-        extendedTimeOut: 5000,
-        iconClasses: {
-          error: 'toast-error',
-          info: 'toast-info',
-          success: 'toast-success',
-          warning: 'toast-warning'
-        },
-        maxOpened: 0,
-        messageClass: 'toast-message',
-        newestOnTop: true,
-        onHidden: null,
-        onShown: null,
-        positionClass: 'toast-bottom-right',
-        preventDuplicates: false,
-        preventOpenDuplicates: false,
-        progressBar: false,
-        tapToDismiss: true,
-        target: 'body',
-        templates: {
-          toast: 'directives/toast/toast.html',
-          progressbar: 'directives/progressbar/progressbar.html'
-        },
-        timeOut: 5000,
-        titleClass: 'toast-title',
-        toastClass: 'toast'
-     });
-});
+   });
