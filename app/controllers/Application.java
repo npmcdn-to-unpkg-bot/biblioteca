4167651519b53235294b833a1cfe2e6216e56443
Haroldo Ramirez da Nóbrega <haroldoramirez@gmail.com>
2016-02-11 16:44:09 -0200

    adicionado cache no index

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import play.cache.Cached;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
 
+    @Cached(key = "homePage")
     public Result index() {
         String username = session().get("email");
         return ok(views.html.index.render(username));
