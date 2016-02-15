1d9dfec3c13b33c799eda0d4d9d0db1915b28bb8
Haroldo Ramirez da Nóbrega <haroldoramirez@gmail.com>
2016-02-15 16:28:50 -0200

    mudado cor do menu superior, e retirado cache do index devido a problemas na hora do login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,7 +6,7 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 
-    @Cached(key = "homePage")
+    //@Cached(key = "homePage")
     public Result index() {
         String username = session().get("email");
         return ok(views.html.index.render(username));
