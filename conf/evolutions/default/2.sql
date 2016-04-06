e9a588ee92e8da9cccdf573191976f26ffe020ba
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-04-06 17:53:52 -0300

    adicionado no validado do admin

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,4 +2,4 @@
 
 # --- !Ups
 
-insert into usuario (nome, email, senha, privilegio, status) values ('Administrador', 'admin', '40BD001563085FC35165329EA1FF5C5ECBDBBEEF', 1, TRUE);
+insert into usuario (nome, email, senha, privilegio, status, validado) values ('Administrador', 'admin', '40BD001563085FC35165329EA1FF5C5ECBDBBEEF', 1, TRUE, TRUE);
