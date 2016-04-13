81e7d85a9b7f162f7d429f9de93f3dc4028fa291
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-04-13 17:06:19 -0300

    iniciando o processo de adicionar os artigos na lista de artigos

--- public/app/controllers/artigo.controller.js
+++ public/app/controllers/artigo.controller.js
@@ -1,5 +1,4 @@
 angular.module('architectplay')
     .controller('artigo.controller', function ($rootScope, $log) {
-        console.log('Controller Artigo');
         $rootScope.title = 'Artigos';
     });
