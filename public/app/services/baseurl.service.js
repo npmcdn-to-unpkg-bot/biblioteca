0268bce88c63086c865c1ec44a53db0ec4542d88
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-07-06 14:19:15 -0300

    alterado base URL obrigado Anderson Davi

--- public/app/services/baseurl.service.js
+++ public/app/services/baseurl.service.js
@@ -1,4 +1,4 @@
 angular.module('architectplay')
   .factory('BaseUrl', function($location) {
-     return 'http://' + $location.host() + ':9000' ;
+     return '//' + $location.host() + ':9000' ;
    });
