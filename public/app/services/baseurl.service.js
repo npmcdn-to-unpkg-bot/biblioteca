bf7ee20e81ad2800cb8386791396130cd74d971b
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-07-06 16:01:49 -0300

    problema trash com o https

--- public/app/services/baseurl.service.js
+++ public/app/services/baseurl.service.js
@@ -1,5 +1,5 @@
 angular.module('architectplay')
   .factory('BaseUrl', function($location) {
      //return 'http://' + $location.host() + ':9000';
-      return '://' + $location.host() + ':9000';
+      return $location.protocol() + '://' + $location.host() + ':9000';
   });
