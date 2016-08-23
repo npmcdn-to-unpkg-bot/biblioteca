66799836af0f7041ac7c33f9e9d531cff0b820d0
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-08-23 17:29:50 -0300

    configurado logback e atualizado play para a versao 2.4.8 e play ebean para a 2.0.0

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,182 +4,192 @@
 # --- !Ups
 
 create table artigo (
-  id                        bigserial not null,
-  titulo                    varchar(150) not null,
-  resumo                    varchar(400) not null,
-  nome_arquivo              varchar(300) not null,
-  data_cadastro             timestamp not null,
-  data_alteracao            timestamp,
-  constraint pk_artigo primary key (id))
-;
+  id                            bigserial not null,
+  titulo                        varchar(150) not null,
+  resumo                        varchar(400) not null,
+  nome_arquivo                  varchar(300) not null,
+  data_cadastro                 date not null,
+  data_alteracao                date,
+  constraint pk_artigo primary key (id)
+);
 
 create table contato (
-  id                        bigserial not null,
-  nome                      varchar(80) not null,
-  email                     varchar(80) not null,
-  assunto                   varchar(50) not null,
-  mensagem                  varchar(400) not null,
-  data_cadastro             timestamp not null,
-  constraint pk_contato primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(80) not null,
+  email                         varchar(80) not null,
+  assunto                       varchar(50) not null,
+  mensagem                      varchar(400) not null,
+  data_cadastro                 date not null,
+  constraint pk_contato primary key (id)
+);
 
 create table curso (
-  id                        bigserial not null,
-  nome                      varchar(100) not null,
-  descricao                 varchar(400) not null,
-  data_inicio               timestamp not null,
-  site                      varchar(300) not null,
-  nome_capa                 varchar(150) not null,
-  constraint pk_curso primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(100) not null,
+  descricao                     varchar(400) not null,
+  data_inicio                   date not null,
+  site                          varchar(300) not null,
+  nome_capa                     varchar(150) not null,
+  constraint pk_curso primary key (id)
+);
 
 create table escolaridade (
-  id                        bigserial not null,
-  nome                      varchar(30) not null,
-  constraint pk_escolaridade primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(30) not null,
+  constraint pk_escolaridade primary key (id)
+);
 
 create table evento (
-  id                        bigserial not null,
-  nome                      varchar(150) not null,
-  data_inicio               timestamp not null,
-  data_fim                  timestamp not null,
-  site                      varchar(300) not null,
-  local                     varchar(150) not null,
-  instituicao               varchar(100) not null,
-  constraint pk_evento primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(150) not null,
+  data_inicio                   date not null,
+  data_fim                      date not null,
+  site                          varchar(300) not null,
+  local                         varchar(150) not null,
+  instituicao                   varchar(100) not null,
+  constraint pk_evento primary key (id)
+);
 
 create table fonte (
-  id                        bigserial not null,
-  nome                      varchar(30) not null,
-  constraint pk_fonte primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(30) not null,
+  constraint pk_fonte primary key (id)
+);
 
 create table genero (
-  id                        bigserial not null,
-  nome                      varchar(14) not null,
-  constraint pk_genero primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(14) not null,
+  constraint pk_genero primary key (id)
+);
 
 create table inscricao (
-  id                        bigserial not null,
-  nome                      varchar(60) not null,
-  genero_id                 bigint,
-  data_nascimento           timestamp not null,
-  escolaridade_id           bigint,
-  profissao                 varchar(30) not null,
-  instituicao               varchar(45) not null,
-  pais_id                   bigint,
-  estado                    varchar(45) not null,
-  cidade                    varchar(45) not null,
-  telefone                  varchar(12) not null,
-  cpf                       varchar(15) not null,
-  email                     varchar(35) not null,
-  modalidade                varchar(3) not null,
-  fonte                     varchar(25) not null,
-  descricao_fonte           varchar(60),
-  data_inscricao            timestamp not null,
-  data_alteracao            timestamp,
-  constraint pk_inscricao primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(60) not null,
+  genero_id                     bigint,
+  data_nascimento               date not null,
+  escolaridade_id               bigint,
+  profissao                     varchar(30) not null,
+  instituicao                   varchar(45) not null,
+  pais_id                       bigint,
+  estado                        varchar(45) not null,
+  cidade                        varchar(45) not null,
+  telefone                      varchar(12) not null,
+  cpf                           varchar(15) not null,
+  email                         varchar(35) not null,
+  modalidade                    varchar(3) not null,
+  fonte                         varchar(25) not null,
+  descricao_fonte               varchar(60),
+  data_inscricao                date not null,
+  data_alteracao                date,
+  constraint pk_inscricao primary key (id)
+);
 
 create table livro (
-  id                        bigserial not null,
-  titulo                    varchar(150) not null,
-  sub_titulo                varchar(250) not null,
-  isbn                      varchar(20),
-  editora                   varchar(100) not null,
-  autores                   varchar(250) not null,
-  edicao                    integer,
-  paginas                   integer,
-  ano                       integer,
-  nome_arquivo              varchar(200) not null,
-  data_cadastro             timestamp not null,
-  data_alteracao            timestamp,
+  id                            bigserial not null,
+  titulo                        varchar(150) not null,
+  sub_titulo                    varchar(250) not null,
+  isbn                          varchar(20),
+  editora                       varchar(100) not null,
+  autores                       varchar(250) not null,
+  edicao                        integer,
+  paginas                       integer,
+  ano                           integer,
+  nome_arquivo                  varchar(200) not null,
+  data_cadastro                 date not null,
+  data_alteracao                date,
   constraint uq_livro_isbn unique (isbn),
-  constraint pk_livro primary key (id))
-;
+  constraint pk_livro primary key (id)
+);
 
 create table modalidade (
-  id                        bigserial not null,
-  nome                      varchar(3) not null,
-  constraint pk_modalidade primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(3) not null,
+  constraint pk_modalidade primary key (id)
+);
 
 create table noticia (
-  id                        bigserial not null,
-  titulo                    varchar(250) not null,
-  resumo                    varchar(400) not null,
-  url                       varchar(400) not null,
-  data_cadastro             timestamp not null,
-  data_alteracao            timestamp,
-  nome_capa                 varchar(250) not null,
-  constraint pk_noticia primary key (id))
-;
+  id                            bigserial not null,
+  titulo                        varchar(250) not null,
+  resumo                        varchar(400) not null,
+  url                           varchar(400) not null,
+  data_cadastro                 date not null,
+  data_alteracao                date,
+  nome_capa                     varchar(250) not null,
+  constraint pk_noticia primary key (id)
+);
 
 create table pais (
-  id                        bigserial not null,
-  nome                      varchar(50) not null,
-  constraint pk_pais primary key (id))
-;
+  id                            bigserial not null,
+  nome                          varchar(50) not null,
+  constraint pk_pais primary key (id)
+);
 
 create table publicacao (
-  id                        bigserial not null,
-  titulo                    varchar(250) not null,
-  resumo                    varchar(400) not null,
-  url                       varchar(400) not null,
-  data_cadastro             timestamp not null,
-  data_alteracao            timestamp,
-  nome_capa                 varchar(250) not null,
-  constraint pk_publicacao primary key (id))
-;
+  id                            bigserial not null,
+  titulo                        varchar(250) not null,
+  resumo                        varchar(400) not null,
+  url                           varchar(400) not null,
+  data_cadastro                 date not null,
+  data_alteracao                date,
+  nome_capa                     varchar(250) not null,
+  constraint pk_publicacao primary key (id)
+);
 
 create table token (
-  token                     varchar(255) not null,
-  usuario_id                bigint,
-  type                      varchar(8),
-  date_creation             timestamp,
-  email                     varchar(255),
+  token                         varchar(255) not null,
+  usuario_id                    bigint,
+  type                          varchar(8),
+  date_creation                 date,
+  email                         varchar(255),
   constraint ck_token_type check (type in ('password','email')),
-  constraint pk_token primary key (token))
-;
+  constraint pk_token primary key (token)
+);
 
 create table usuario (
-  id                        bigserial not null,
-  confirmacao_token         varchar(255),
-  validado                  boolean,
-  nome                      varchar(60) not null,
-  email                     varchar(40) not null,
-  senha                     varchar(60) not null,
-  privilegio                integer not null,
-  status                    boolean not null,
-  data_cadastro             timestamp,
-  data_alteracao            timestamp,
+  id                            bigserial not null,
+  confirmacao_token             varchar(255),
+  validado                      boolean,
+  nome                          varchar(60) not null,
+  email                         varchar(40) not null,
+  senha                         varchar(60) not null,
+  privilegio                    integer not null,
+  status                        boolean not null,
+  data_cadastro                 date,
+  data_alteracao                date,
   constraint uq_usuario_email unique (email),
-  constraint pk_usuario primary key (id))
-;
+  constraint pk_usuario primary key (id)
+);
 
 create table video (
-  id                        bigserial not null,
-  titulo                    varchar(250) not null,
-  descricao                 varchar(400) not null,
-  url                       varchar(400) not null,
-  data_cadastro             timestamp not null,
-  data_alteracao            timestamp,
-  constraint pk_video primary key (id))
-;
+  id                            bigserial not null,
+  titulo                        varchar(250) not null,
+  descricao                     varchar(400) not null,
+  url                           varchar(400) not null,
+  data_cadastro                 date not null,
+  data_alteracao                date,
+  constraint pk_video primary key (id)
+);
 
-alter table inscricao add constraint fk_inscricao_genero_1 foreign key (genero_id) references genero (id);
-create index ix_inscricao_genero_1 on inscricao (genero_id);
-alter table inscricao add constraint fk_inscricao_escolaridade_2 foreign key (escolaridade_id) references escolaridade (id);
-create index ix_inscricao_escolaridade_2 on inscricao (escolaridade_id);
-alter table inscricao add constraint fk_inscricao_pais_3 foreign key (pais_id) references pais (id);
-create index ix_inscricao_pais_3 on inscricao (pais_id);
+alter table inscricao add constraint fk_inscricao_genero_id foreign key (genero_id) references genero (id) on delete restrict on update restrict;
+create index ix_inscricao_genero_id on inscricao (genero_id);
 
+alter table inscricao add constraint fk_inscricao_escolaridade_id foreign key (escolaridade_id) references escolaridade (id) on delete restrict on update restrict;
+create index ix_inscricao_escolaridade_id on inscricao (escolaridade_id);
+
+alter table inscricao add constraint fk_inscricao_pais_id foreign key (pais_id) references pais (id) on delete restrict on update restrict;
+create index ix_inscricao_pais_id on inscricao (pais_id);
 
 
 # --- !Downs
+
+alter table inscricao drop constraint if exists fk_inscricao_genero_id;
+drop index if exists ix_inscricao_genero_id;
+
+alter table inscricao drop constraint if exists fk_inscricao_escolaridade_id;
+drop index if exists ix_inscricao_escolaridade_id;
+
+alter table inscricao drop constraint if exists fk_inscricao_pais_id;
+drop index if exists ix_inscricao_pais_id;
 
 drop table if exists artigo cascade;
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,6 @@
 // The Play plugin
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.6")
-addSbtPlugin("com.typesafe.sbt" % "sbt-play-ebean" % "1.0.0")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.8")
+addSbtPlugin("com.typesafe.sbt" % "sbt-play-ebean" % "2.0.0")
 
 // Web plugins
 addSbtPlugin("com.typesafe.sbt" % "sbt-coffeescript" % "1.0.0")
